87a7dd57133ff8688bb711307af642c869294fe4
Eric <xyan0147@student.monash.edu>
2025-03-16 21:06:25 +0800

    Refactor Quick_sort to include multiple partition schemes: Lomuto, Hoare, and Dutch National Flag

--- Sorting/Quick_sort.py
+++ Sorting/Quick_sort.py
@@ -1,4 +1,4 @@
-from typing import TypeVar, Callable, Iterable
+from typing import TypeVar, Callable, Iterable, Tuple
 
 T = TypeVar('T')
 
@@ -22,9 +22,13 @@ def Quick_sort(ln_struct: Iterable[T], key: Callable = lambda x: x, reverse: boo
     reverse: If True, the structure is sorted in descending order.
     """
 
-    def partition(left: int, right: int) -> int:
+    def partition_lumutos(left: int, right: int) -> int:
         """
         Partitions the list around a pivot element.
+        This is Lomuto's partition scheme.
+        We choose the rightmost element as the pivot, and place it in its correct position.
+        Then we partition the list into two sublists, one with elements less than the pivot,
+        and the other with elements greater than the pivot. We return the index of the pivot.
         """
         # Choose the rightmost element as the pivot
         pivot = ln_struct[right]
@@ -40,6 +44,65 @@ def Quick_sort(ln_struct: Iterable[T], key: Callable = lambda x: x, reverse: boo
         # Place the pivot element in its correct position
         swap(ln_struct, i + 1, right)
         return i + 1
+    
+    # the other ways to implement partition are Hoare's partition scheme and Dutch National Flag algorithm
+    # Hoare's partition scheme is more efficient than Lomuto's partition scheme, it is not stable though
+    def partition_hoares(left: int, right: int) -> int:
+        """Hoare's partition scheme"""
+        pivot = ln_struct[left]  # typically uses leftmost element
+        i = left - 1
+        j = right + 1
+        
+        while True:
+            # Find element on left that should be on right
+            i += 1
+            while i < right and (key(ln_struct[i]) < key(pivot)) != reverse:
+                i += 1
+                
+            # Find element on right that should be on left
+            j -= 1
+            while j > left and (key(ln_struct[j]) > key(pivot)) != reverse:
+                j -= 1
+                
+            # If pointers crossed, return
+            if i >= j:
+                return j
+                
+            # Swap elements
+            swap(ln_struct, i, j)
+        # Dutch National Flag algorithm is more efficient than Hoare's partition scheme, but it is more complex and not stable
+    
+    def partition_dutch_national_flag(left: int, right: int) -> Tuple[int, int]:
+        """Dutch National Flag algorithm"""
+        pivot = ln_struct[right]
+        low = left - 1
+        high = right
+        i = left - 1
+        j = right
+        while True:
+            while i < right and (key(ln_struct[i]) < key(pivot)) != reverse:
+                i += 1
+            while j > left and (key(ln_struct[j]) > key(pivot)) != reverse:
+                j -= 1
+            if i >= j:
+                break
+            swap(ln_struct, i, j)
+            if key(ln_struct[i]) == key(pivot):
+                low += 1
+                swap(ln_struct, low, i)
+            if key(ln_struct[j]) == key(pivot):
+                high -= 1
+                swap(ln_struct, high, j)
+        swap(ln_struct, i, right)
+        j = i - 1
+        for k in range(left, low):
+            swap(ln_struct, k, j)
+            j -= 1
+        i = i + 1
+        for k in range(right - 1, high, -1):
+            swap(ln_struct, i, k)
+            i += 1
+        return j, i
 
     def quick_sort(left: int, right: int) -> None:
         """
@@ -47,7 +110,7 @@ def Quick_sort(ln_struct: Iterable[T], key: Callable = lambda x: x, reverse: boo
         """
         if left < right:
             # Partition the list
-            pivot = partition(left, right)
+            pivot = partition_lumutos(left, right)
 
             # Recursively sort the left and right sublists
             quick_sort(left, pivot - 1)
@@ -55,3 +118,7 @@ def Quick_sort(ln_struct: Iterable[T], key: Callable = lambda x: x, reverse: boo
 
     # Call the recursive quick_sort function
     quick_sort(0, len(ln_struct) - 1)
+    
+    
+
+
